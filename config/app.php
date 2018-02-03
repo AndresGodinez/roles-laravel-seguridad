0322322d01616cc282ef575b6223e02593e997b9
Andres Vagrant <ing.a.godinez@gmail.com>
2018-02-03 05:25:35 +0000

    Añadiendo librerías Laravel Collective y shinobi

--- config/app.php
+++ config/app.php
@@ -164,6 +164,13 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        // Laravel collective
+        Collective\Html\HtmlServiceProvider::class,
+
+        // Shinobi certificado para roles
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
+
+
         /*
          * Package Service Providers...
          */
@@ -225,6 +232,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Laravel collective 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
